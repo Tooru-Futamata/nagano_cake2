8a8f5ba1e4e317b01d12d5adbaabeba4661e551a
EC2 Default User <ec2-user@ip-172-31-85-57.ec2.internal>
2021-10-04 13:14:40 +0000

    comment

--- db/migrate/20210928065745_add_sale_status_to_items.rb
+++
@@ -1,5 +0,0 @@
-class AddSaleStatusToItems < ActiveRecord::Migration[5.2]
-  def change
-    add_column :items, :sale_status, :integer
-  end
-end
--- db/migrate/20210928095929_add_withdrawal_status_to_customers.rb
+++
@@ -1,5 +0,0 @@
-class AddWithdrawalStatusToCustomers < ActiveRecord::Migration[5.2]
-  def change
-    add_column :customers, :withdrawal_status, :string
-  end
-end
