c482baaad66b80447391f482753771c6b1731ef5
EC2 Default User <ec2-user@ip-172-31-86-242.ec2.internal>
2021-10-14 18:07:52 +0000

    finish

--- app/models/order_detail.rb
+++ app/models/order_detail.rb
@@ -5,7 +5,12 @@ class OrderDetail < ApplicationRecord
   validates :item_id, :order_id, presence: true
   validates :price, :amount, numericality: { only_integer: true }
 
-  enum making_status: { 製作不可: 0, 製作待ち: 1, 製作中: 2, 製作済み: 3 }
+  enum making_status: {
+    製作不可: '製作不可',
+    製作待ち: '製作待ち',
+    製作中: '製作中',
+    製作完了: '製作完了'
+  }
 
   def subtotal
     self.item.price * self.amount
